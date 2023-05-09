531e1d143255ddfbd6bcf78ad1f6b8fec3c7a2a7
Shania Lopez <slopez@fandm.edu>
2023-05-08 20:45:10 -0400

    could not get 1/7 tests to pass

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let startIndex = 0; startIndex < array.length; startIndex ++){
+    for(let endIndex = array.length - 1; endIndex > 0; endIndex --){
+      if(array[startIndex] + array[endIndex] === target)
+      return true;
+    }  
+    return false;
+  }
+  //return false;
 }
+
+
 
 /* 
   Write the Big O time complexity of your function here
+  nested for loop is O(n squared)
 */
 
 /* 
   Add your pseudocode here
+takes an array and a target as parameters
+iterates over the array
+if two elements in the array can add up to the target
+return true
+
+[1,2,3,4,5], 6
+^        ^
+  ^    ^   return true 
+
+  two iterations, 
+  startIndex 
+  end Index 
+  if start + end = target, 
+  return true
 */
 
 /*
